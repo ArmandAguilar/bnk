b2c4ce88a9c0745252c821f92853aeca025a026d
Armando Aguilar L <a.aguilar@gategeek.com>
2018-02-26 17:24:11 -0600

    i test in production

---
+++ scripts/main.py
@@ -0,0 +1,33 @@
+#!user/bin/python
+# -*- coding: utf-8 -*-
+import pandas as pd
+#other lib and toke to use
+from tokensSQL import *
+from tokensNotification import *
+from notifications import *
+from read_xls import *
+import pypyodbc as pyodbc
+import pymssql
+import urllib2, base64
+import simplejson as json
+import unicodedata
+import sys
+reload(sys)
+
+
+################################################################################
+##                                                                            ##
+##                              Main                                          ##
+##                                                                            ##
+################################################################################
+
+#Here Json with all accoutn of bank an box
+AccountsJson = '{"Person":['
+AccountsJson += '{"Table":"AA2367045623Bnx","Sheet":"2367045623Banamex","URL":"H://Dropbox//Personal//Dropbox (Personal)//Tesoreria//3.-Bnk//2367045623Banamex-1-A.xlsm","Dates":"2018-01-01"},' + '\n'
+AccountsJson += '{"Table":"AA2367057222Bnx","Sheet":"Hoja1","URL":"H://Dropbox//Personal//Dropbox (Personal)//Tesoreria//3.-Bnk//2367057222Banamex-1B.xls","Dates":"2018-01-01"}' + '\n'
+AccountsJson += ']}'
+data = json.loads(AccountsJson,strict=False)
+print('====== >> Procensado')
+for value in data['Person']:
+    readXls(str(value['URL']),str(value['Table']),str(value['Dates']),str(value['Sheet']))
+    print('Procesnado :' + str(value['Table']))
---
+++ scripts/read_xls.py
@@ -0,0 +1,61 @@
+#!user/bin/python
+# -*- coding: utf-8 -*-
+#Here import Libs Here
+## Here import libs
+import pandas as pd
+#other lib and toke to use
+from tokensSQL import *
+from tokensNotification import *
+from notifications import *
+import pypyodbc as pyodbc
+import pymssql
+
+
+def list_bnk(bkAccount,dateCompare):
+    listBnk = []
+    k = 0
+    sql = 'SELECT [Id_Oper] FROM [SAP].[dbo].[' + str(bkAccount) + '] Where Fecha >= \'' + str(dateCompare) + '\''
+    conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
+    cur = conn.cursor()
+    cur.execute(sql)
+    for value in cur:
+        Id_Opers = value[0]
+        listBnk.insert(k,str(Id_Opers))
+    conn.commit()
+    conn.close()
+    return listBnk
+
+def list_xls(xls_url,sheetname):
+    dataframeBnk = pd.read_excel(str(xls_url),sheet_name=str(sheetname),na_values='n/a')
+    newdataframeBnk = dataframeBnk[dataframeBnk['Fecha'] >= '2018-01-01']
+    dataFrameIdOpersXLS = newdataframeBnk['Id_Oper']
+    listXLS = dataFrameIdOpersXLS.tolist()
+    return listXLS
+
+
+def readXls(fileX,bkn,dates,sheetname):
+    listXLS = list_xls(fileX,sheetname)
+    listSQL = list_bnk(bkn,dates)
+    i = 0
+    message = '################################################################\n'
+    message += '\t Registros faltantes de  la cuenta : ' + str(bkn) + '\n'
+    message += '################################################################\n'
+    #print('Id_opers do not found in SQL SERVER')
+    for value in listXLS:
+        val = 'Bad'
+        for value1 in listSQL:
+            if value == value1:
+                val = 'Good'
+        if val == 'Bad':
+            i = 1 + i
+            message += str(i) + '.- ' + ' Id_Oper :'+ value + '\n'
+            #print (str(i) + '.- ' + ' Id_Oper :'+ value)
+    message += "################################################################\n"
+    title = 'Cuenta analizada : ' + str(bkn)
+    send_notification(title,'a.aguilar@fortaingenieria.com',message)
+    send_notification(title,'administracion@fortaingenieria.com',message)
+################################################################################
+##                                                                            ##
+##                                  Test Here code                            ##
+##                                                                            ##
+################################################################################
